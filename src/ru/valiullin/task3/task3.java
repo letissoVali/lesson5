08b314826396acd3e09dd24fad3cb3c5595bc18c
Iskander <valiullin00@mail.ru>
2018-08-26 06:14:20 +0300

    commit task2

--- src/ru/valiullin/task3/task3.java
+++ src/ru/valiullin/task3/task3.java
@@ -1,4 +1,15 @@
 package ru.valiullin.task3;
 
+
+//todo Написать программу для вывода на экран таблицы умножения. Подсказка нужно использовать цикл в цикле
 public class task3 {
+    public static void main(String[] args) {
+        System.out.println("Таблица умножения:");
+        for (int i = 1; i < 10; i++) {
+            for(int j = 1; j < 10; j++) {
+                System.out.println("Число "+ i +" умножаем на " +j + " равно " + i*j);
+            }
+            System.out.println("----------------------");
+        }
+    }
 }
